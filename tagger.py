869cea37663cf0d0e4691738f8c5b8de0b0d2232
samyak-jain <samtan106@gmail.com>
2019-05-01 04:26:13 +0530

    Started with query construction

--- tagger.py
+++ tagger.py
@@ -1,16 +1,17 @@
 import requests
 import urllib.parse
-from mutagen.mp3 import MP3
-from mutagen.id3 import ID3, APIC, error
-from typing import Dict, List, Any, Optional
+from typing import Dict, List, Any, Optional, Tuple
 from pathlib import Path
 import os
+from tqdm import tqdm
 
 BASE_URL = "https://vgmdb.info"
 SEARCH_URL = f"{BASE_URL}/search"
 
-def query_vgmdb(query: str) -> Dict[str, str]:
-	song_name: str
+def query_vgmdb(query: str) -> Optional[Dict[str, Optional[str]]]:
+	song_name: Optional[str] = None
+	artists: Optional[str] = None
+	album_art: Optional[str] = None
 
 	parsed_query: str = urllib.parse.quote(query)
 	search_result: Dict[str, Any] = requests.get(f"{SEARCH_URL}/{parsed_query}?format=json").json()
@@ -19,9 +20,11 @@ def query_vgmdb(query: str) -> Dict[str, str]:
 	for album in albums:
 		album_code: str = album["link"]
 		album_details: Dict[str, Any] = requests.get(f"{BASE_URL}/{album_code}?format=json").json()
-		album_art: str = album_details["picture_full"]
+		album_art = album_details["picture_full"]
 
 		# Ignore albums with no album art or track listings
+
+		assert album_art is not None
 		if "nocover" in album_art or len(album_details["discs"]) == 0:
 			continue
 
@@ -58,9 +61,12 @@ def query_vgmdb(query: str) -> Dict[str, str]:
 
 			artist_list.append(name)
 
-		artists: str = ", ".join(artist_list)
+		artists = ", ".join(artist_list)
 
 		break
+
+	if (song_name is None) or (artists is None) or (album_art is None):
+		return None
 
 	return {
 		"Name": song_name,
@@ -68,30 +74,49 @@ def query_vgmdb(query: str) -> Dict[str, str]:
 		"Album Art": album_art
 	}
 
+def get_all_possible_substrings(query: str, length: int) -> List[str]:
+	substrings: List[str] = []
+	query_length: int = len(query)
 
-def construct_query():
-	pass
+	assert query_length >= length
+	for start_index in range(query_length - length):
+		substrings.append(query[start_index:start_index+length])
+
+	return substrings
+
+def construct_query(query: str) -> Optional[Tuple[str, Optional[Dict[str, Optional[str]]]]]:
+	longest_query: List[str] = [query]
+
+	for length in tqdm(range(len(query)-1, 1, -1)):
+		for index, long_query in enumerate(longest_query):
+
+			result: Optional[Dict[str, Optional[str]]] = query_vgmdb(long_query)
+			if result is not None:
+				return long_query, result
+
+		longest_query = get_all_possible_substrings(query, length)
+		print(longest_query)
+
+	return None
+
+
+
+
 
 
 def tag_song(path: Path, song: str):
-	audio = MP3(str(path / song), ID3=ID3)    
-	query: str = os.path.splitext(song)[0]
-	meta: Dict[str, str] = query_vgmdb(query)
-	audio.tags.add(
-	    APIC(
-	        encoding=3, # 3 is for utf-8
-	        mime='image/jpg', # image/jpeg or image/png
-	        type=3, # 3 is for the cover image
-	        desc=u'Cover',
-	        data=requests.get(meta["Album Art"]).content
-	    )
-	)
-
-	audio.save()
+	pass
 
 if __name__=="__main__":
-	# print(query_vgmdb("アイシテル"))
+#	print(query_vgmdb("アイシテル"))
 	path = Path("/home/samyak/music_test")
 	files = os.listdir(str(path))
-	for file in files:
-		tag_song(path, file)
+	file_names = [os.path.splitext(file_name)[0] for file_name in files]
+	print(file_names)
+	for file_name in file_names:
+		result = construct_query(file_name)
+		if result is None:
+			print(f"Attempt at tagging {file_name} failed")
+		else:
+			print(f"Tagging successfull. The resultant query is {result[0]}")
+			print(f"And the result is {result[1]}")
