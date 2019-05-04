818675e79cdbfa709d79d3919f6149f861823a99
samyak-jain <samtan106@gmail.com>
2019-05-05 05:02:58 +0530

    Started code for fetching videos

--- fetchvids.py
+++ fetchvids.py
@@ -1,2 +1,8 @@
 import subprocess
 
+
+def get_vid_list():
+    command = "youtube-dl --cookies cookie_secret.txt " \
+              "'https://www.youtube.com/watch?v=Q9WcG0OMElo&list=RDGMEMhCgTQvcskbGUxqI4Sn2QYw&start_radio=1' " \
+              "--flat-playlist -j | jq -r '.id' | sed 's_^_https://youtu.be/_' "
+
