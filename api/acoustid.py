ac0dd5611c2b88ffdd75677754fd6c28d9cf4983
samyak-jain <samtan106@gmail.com>
2019-05-05 07:00:30 +0530

    Added interaction with server

--- api/acoustid.py
+++ api/acoustid.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+import urllib.parse
+from typing import Dict
+
+import requests
+from requests import Response
 
 
 class ACOUSTID:
@@ -7,4 +12,14 @@ class ACOUSTID:
         self.server_url = server_url
 
     def inference(self):
-        pass
+        absolute_path: str = str(self.song_path.absolute())
+        encoded_path: str = urllib.parse.quote(absolute_path)
+
+        response: Response = requests.get(f"{self.server_url}/tag/{encoded_path}")
+
+        if response.status_code != 200:
+            print("There was some kind of error")
+            print(f"Error: {response.status_code}")
+            return None
+
+        acoust: Dict[str, str] = response.json()
